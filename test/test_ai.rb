1a9a5e55e966e4757c9757b2c43a5bcd3637e002
Christian Blais <christ.blais@gmail.com>
2011-05-20 00:28:18 -0400

    Refactor test as Sinatra now goes directly into the gem

--- test/test_ai.rb
+++ test/test_ai.rb
@@ -1,20 +1,14 @@
 require 'rubygems'
-require 'sinatra'
-require 'yajl/json_gem'
 require 'berlin-ai'
 
-module Berlin
-  module AI
-    class Game
-      def turn_moves
-        @map.controlled_nodes.map do |node|
-          {
-            :from => node.id,
-            :to => node.adjacent_nodes.sample.id,
-            :number_of_soldiers => rand(node.number_of_soldiers + 1)
-          }
-        end
-      end
+class Berlin::AI::Game
+  def turn_moves
+    @map.controlled_nodes.map do |node|
+      {
+        :from => node.id,
+        :to => node.adjacent_nodes.sample.id,
+        :number_of_soldiers => rand(node.number_of_soldiers + 1)
+      }
     end
   end
 end
