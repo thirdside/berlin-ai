12668a4d6c688349676702296a965fde78ccd5d2
Christian Blais <christ.blais@gmail.com>
2011-05-24 19:00:42 -0400

    Bump version

--- lib/version.rb
+++ lib/version.rb
@@ -2,7 +2,7 @@ module Berlin
   module AI
     MAJOR = 0
     MINOR = 0
-    BUILD = 12
+    BUILD = 13
 
     VERSION = "#{MAJOR}.#{MINOR}.#{BUILD}"
   end
--- test/test_ai.rb
+++ test/test_ai.rb
@@ -3,6 +3,8 @@ require 'berlin-ai'
 
 class Berlin::AI::Player
   def self.on_turn( game )
+    
+    
     game.add_move( 1, 2, 12 )
   end
 end
